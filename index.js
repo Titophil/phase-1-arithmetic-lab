d62262c937f7b7825efe52b10b4bd2ed919d9a44
titophil <kipronotitus254@gmail.com>
2025-03-07 12:42:58 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 2
+const num2 = 31
 
-// Write your code here
+let multiply = num1*num2
+console.log(multiply)
+
+let random = Math.floor(Math.random()*10)+1
+console.log(random)
+
+const num3 = 10
+const num4 = 6
+
+let mod = num3%num4
+console.log(mod)
+
+let max = Math.max(1,2,3,20)
+console.log(max)
